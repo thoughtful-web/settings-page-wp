2fd78b740521518217c786a72605e6c48d6d46ba
Zachary K Watkins <3458875+ZachWatkins@users.noreply.github.com>
2022-01-11 13:30:01 -0600

    Apply the better format of sanitization to the Text_Validator class

--- src/Admin/Page/Settings/Validate/Text_Validator.php
+++ src/Admin/Page/Settings/Validate/Text_Validator.php
@@ -41,8 +41,13 @@ class Text_Validator extends Validate {
 	 */
 	public function is_valid( $input ) {
 
-		$label = $this->settings['label'];
-		$valid = array(
+		// Declare initial variables.
+		$initial_value = $input;
+		$label         = $this->settings['label'];
+		// Declare success based variables.
+		$success = 'The ' . $label . ' value is valid.';
+		$failure = 'The ' . $label . ' value has one or more validation errors:';
+		$valid   = array(
 			'status'  => true,
 			'message' => array(),
 		);
@@ -52,7 +57,7 @@ class Text_Validator extends Validate {
 		if ( ! empty( $this->settings['data_args']['required'] ) ) {
 			if ( true === $is_empty['status'] ) {
 				$valid['status']    = false;
-				$valid['message'][] = 'The value cannot be empty.';
+				$valid['message']['is_empty'] = 'The value cannot be empty.';
 			}
 		}
 
@@ -61,25 +66,31 @@ class Text_Validator extends Validate {
 			$is_pattern = $this->is_pattern( $input );
 			if ( false === $is_pattern['status'] ) {
 				$valid['status']    = false;
-				$valid['message'][] = 'The value must follow the pattern "' . $this->settings['data_args']['pattern'] . '"';
+				$valid['message']['is_pattern'] = 'The value must follow the pattern "' . $this->settings['data_args']['pattern'] . '"';
 			}
 		}
 
 		// If the input must follow length requirements.
 		$is_length = $this->is_length( $input );
 		if ( false === $is_length['status'] ) {
-			$valid['status']    = false;
-			$valid['message'][] = $is_length['message'];
+			$valid['status']               = false;
+			$valid['message']['is_length'] = $is_length['message'];
 		}
 
-		// Combine error messages.
-		if ( ! empty( $valid['message'] ) ) {
-			// Insert the description at the beginning of the array.
-			$description = apply_filters( 'twl_settings_validate_text', 'The ' . $label . ' setting has one or more validation errors:', $label, $this->settings );
-			array_unshift( $valid['message'], $description );
-			// Concatenate the validation message string.
-			$valid['message'] = implode( ' ', $valid['message'] );
+		// Load the initial value of the results message.
+		if ( $input === $initial_value ) {
+			// Set the success message.
+			$valid['message']['success'] = $success;
+		} else {
+			// Set the failure message preface.
+			array_unshift( $valid['message'], $failure );
 		}
+
+		// Apply user filters to the return value. Typically used to customize messages.
+		$valid = apply_filters( 'twl_settings_validate_text', $valid, $label, $this->settings );
+
+		// Convert the message to a single string.
+		$valid['message'] = implode( ' ', $valid['message'] );
 
 		return $valid;
 
@@ -100,9 +111,11 @@ class Text_Validator extends Validate {
 		);
 
 		// I suspect 16mb is a common max string length for SQL database insertion on shared hosting for WordPress.
-		$true_max  = 16777216;
+		if ( ! defined( 'TWL_SETTINGS_MAX_STRING_LENGTH' ) ) {
+			define( 'TWL_SETTINGS_MAX_STRING_LENGTH', 16777216 );
+		}
 		$minlength = 0;
-		$maxlength = apply_filters( 'twl_settings_max_string_length', $true_max );
+		$maxlength = TWL_SETTINGS_MAX_STRING_LENGTH;
 		$strlen    = strlen( $input );
 		if ( ! empty( $this->settings['data_args']['minlength'] ) ) {
 			$minlength = $this->settings['data_args']['minlength'];
@@ -150,6 +163,33 @@ class Text_Validator extends Validate {
 		}
 
 		return $valid;
+
+	}
+
+	/**
+	 * Validate the emptiness of the input.
+	 *
+	 * @param  string $input The setting input value.
+	 * @return array
+	 */
+	public function is_empty( $input ) {
+
+		$label = $this->settings['label'];
+		$valid = array(
+			'status'  => false,
+			'message' => 'The ' . $label . ' field is not empty.'
+		);
+
+		// Validate.
+		if ( empty( $input ) || empty( trim( $input ) ) ) {
+
+			$valid['status']  = true;
+			$valid['message'] = 'The ' . $label . ' setting is empty.';
+
+		}
+
+		return $valid;
+
 	}
 
 }
