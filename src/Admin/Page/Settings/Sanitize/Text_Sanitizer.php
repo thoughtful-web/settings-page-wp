28a7482ee1b7686cc1bce513621cfd8502c5b3ef
Zachary K Watkins <3458875+ZachWatkins@users.noreply.github.com>
2022-01-11 05:53:23 -0600

    Extend the Sanitize class into a Text_Sanitizer class

---
+++ src/Admin/Page/Settings/Sanitize/Text_Sanitizer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The file that creates a Settings page section.
+ *
+ * @package    ThoughtfulWeb\LibraryWP
+ * @subpackage Settings
+ * @author     Zachary Kendall Watkins <zachwatkins@tapfuel.io>
+ * @copyright  2022 Zachary Kendall Watkins
+ * @license    https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0-or-later
+ * @link       https://github.com/thoughtful-web/library-wp/blob/master/admin/page/settings/validate.php
+ * @since      0.1.0
+ */
+
+declare(strict_types=1);
+namespace ThoughtfulWeb\LibraryWP\Admin\Page\Settings;
+
+use \ThoughtfulWeb\LibraryWP\Admin\Page\Settings\Sanitize;
+
+class Text_Sanitizer extends Sanitize {
+
+}
