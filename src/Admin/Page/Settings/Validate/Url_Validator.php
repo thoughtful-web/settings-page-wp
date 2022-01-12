bdf3bd6d107891d65c8f881baa1fc604665e4288
Zachary K Watkins <3458875+ZachWatkins@users.noreply.github.com>
2022-01-12 13:04:24 -0600

    Correct indentation

--- src/Admin/Page/Settings/Validate/Url_Validator.php
+++ src/Admin/Page/Settings/Validate/Url_Validator.php
@@ -35,7 +35,7 @@ class Url_Validator extends Text_Validator {
 		$is_empty = $this->is_empty( $input );
 		if ( ! empty( $this->settings['data_args']['required'] ) ) {
 			if ( true === $is_empty['status'] ) {
-				$valid['status']    = false;
+				$valid['status']              = false;
 				$valid['message']['is_empty'] = 'The value cannot be empty.';
 			}
 		}
@@ -44,7 +44,7 @@ class Url_Validator extends Text_Validator {
 		if ( ! empty( $this->settings['data_args']['pattern'] ) ) {
 			$is_pattern = $this->is_pattern( $input );
 			if ( false === $is_pattern['status'] ) {
-				$valid['status']    = false;
+				$valid['status']                 = false;
 				$valid['message']['not_pattern'] = 'The value must follow the pattern "' . $this->settings['data_args']['pattern'] . '"';
 			}
 		}
@@ -52,7 +52,7 @@ class Url_Validator extends Text_Validator {
 		// If the input must follow length requirements.
 		$is_length = $this->is_length( $input );
 		if ( false === $is_length['status'] ) {
-			$valid['status']               = false;
+			$valid['status']                = false;
 			$valid['message']['not_length'] = $is_length['message'];
 		}
 
