6ed0f2bca4990d77f73529b637bcc7336d4a2bd5
Zachary K Watkins <3458875+ZachWatkins@users.noreply.github.com>
2022-02-11 06:58:47 -0600

    Remove the release bin file and move it to the bin folder

--- hooks/post-commit.ps1
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env pwsh
-# Create the release package.
-$tag=@(git describe --abbrev=0 --tags)
-tar -a -cf "settings-page-wp-$tag.zip" config src composer.json AUTHORS LICENSE README.md SECURITY.md 
